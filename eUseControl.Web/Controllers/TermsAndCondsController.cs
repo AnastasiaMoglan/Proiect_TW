749974b4edb2308ca4b3b0191769009be4556c2d
AnastasiaMoglan <anastasia.moglan@isa.utm.md>
2025-05-10 23:06:18 +0300

    MedicalConsultation&TermsAndConditions

---
+++ eUseControl.Web/Controllers/TermsAndCondsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eUseControl.Web.Controllers
+{
+    public class TermsAndCondsController : Controller
+    {
+        // GET: TermsAndConds
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ eUseControl.Web/Controllers/TermsController.cs
@@ -0,0 +1,12 @@
+using System.Web.Mvc;
+
+namespace eUseControl.Web.Controllers
+{
+    public class TermsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+} 

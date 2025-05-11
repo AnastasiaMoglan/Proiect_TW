3abb1f45c8cfc79dd88e505bf49632ed68a1dd31
AnastasiaMoglan <anastasia.moglan@isa.utm.md>
2025-05-11 13:09:32 +0300

    OurTeam

---
+++ eUseControl.Web/Controllers/OurTeamController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eUseControl.Web.Controllers
+{
+    public class OurTeamController : Controller
+    {
+        // GET: OurTeam
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

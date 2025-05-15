6572d61339285a223b5e92ddbbdbe03b9c32df7a
AnastasiaMoglan <anastasia.moglan@isa.utm.md>
2025-05-15 22:12:30 +0300

    LoginPage

--- eUseControl.Web/Controllers/AccountController.cs
+++ eUseControl.Web/Controllers/AccountController.cs
@@ -13,5 +13,39 @@ namespace eUseControl.Web.Controllers
         {
             return View();
         }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string email, string password)
+        {
+            // TODO: Add authentication logic here
+            // If successful:
+            // return RedirectToAction("Index", "Home");
+            // If failed:
+            ViewBag.Error = "Invalid email or password.";
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(string name, string username, string email, string password, string confirmPassword, string phone)
+        {
+            // TODO: Add registration logic here
+            // If successful:
+            // return RedirectToAction("Login");
+            // If failed:
+            ViewBag.Error = "Registration failed.";
+            return View();
+        }
     }
 }
